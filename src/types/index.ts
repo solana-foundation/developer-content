aeaa4a2294df173dcb13a62e490ba5d1778b60b3
nickfrosty <nick.frostbutter@solana.org>
2023-07-20 11:04:12 -0400

    refactor: short hand type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,13 @@
  *
  */
 
+import { DocumentTypes, IgnoredDoc } from "contentlayer/generated";
+
+/**
+ * Short hand for removing the IgnoredDoc
+ */
+export type SupportedDocTypes = Exclude<DocumentTypes, IgnoredDoc>;
+
 /**
  * Simplified string names of each support content record group
  *
