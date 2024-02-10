e19504606f7eb6589f4bc9a0e908f5ce91d060ab
nickfrosty <nfrostbutter@gmail.com>
2024-02-10 11:29:50 -0700

    feat: track desired state to include i18n in paths

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,3 +12,13 @@ export const LOCALE_REGEX = /^\w{2,2}$/gi;
  * Regex to parse the i18n locale for content records
  */
 export const I18N_LOCALE_REGEX = new RegExp(/^i18n\/(\w{2,2})\//i);
+
+/**
+ * Whether or not to include the `/i18n/{locale}` prefix on the contentlayer
+ * computed `_id` and `_raw` attributes
+ *
+ * Generally, if the multi-locale content is committed to the repo
+ * (i.e. downloaded via Crowdin at build time) this should be `true`.
+ * if the locale content is NOT committed, this should be `false`
+ */
+export const I18N_INCLUDE_IN_PATHS = false;
