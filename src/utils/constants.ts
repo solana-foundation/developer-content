0b3366fc3c357ffa7da1c73e4370f6d76b788fe2
nickfrosty <nfrostbutter@gmail.com>
2024-02-10 10:08:00 -0700

    fix: missing locale regex

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,8 @@ export const DEFAULT_LOCALE_EN = "en";
  * Standard RegEx to parse a 2 character locale code
  */
 export const LOCALE_REGEX = /^\w{2,2}$/gi;
+
+/**
+ * Regex to parse the i18n locale for content records
+ */
+export const I18N_LOCALE_REGEX = new RegExp(/^i18n\/(\w{2,2})\//i);
