1558254095f41da54b10129b24fc10b3f745909a
Mike MacCana <mike.maccana@gmail.com>
2024-10-03 13:45:12 +1000

    Delete content/courses/onchain-development/temp.rs

--- content/courses/onchain-development/temp.rs
+++
@@ -1,9 +0,0 @@
-#[program]
-mod program_module_name {
-    use super::*;
-
-    pub fn instruction_one(ctx: Context<InstructionAccounts>, instruction_data: u64) -> Result<()> {
-        ctx.accounts.account_name.data = instruction_data;
-        Ok(())
-    }
-}
